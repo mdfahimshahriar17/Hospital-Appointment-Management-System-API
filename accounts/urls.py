0ecece3b7d30eae5ceddb67ab715c0b26a20ea36
fshahriar298 <mdfahimshahriar17@gmail.com>
2026-09-15 03:28:20 +0600

    Added Login URL

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .views import RegisterView
 
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
 ]
