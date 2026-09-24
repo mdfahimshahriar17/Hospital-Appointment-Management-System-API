4fc1f027233713c1a82178bfac18ce824c09de70
fshahriar298 <mdfahimshahriar17@gmail.com>
2026-09-24 19:34:57 +0600

    Created Doctor Serializer

--- doctors/models.py
+++ doctors/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Doctor(models.Model):
-    name = models.CharField()
-    department = models.CharField()
-    specialization = models.CharField()
-    visiting_fee = models.DecimalField()
+    name = models.CharField(max_length=100)
+    department = models.CharField(max_length=100)
+    specialization = models.CharField(max_length=100)
+    visiting_fee = models.DecimalField(max_digits=10, decimal_places=2, default=0)
---
+++ doctors/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Doctor
+
+
+class DoctorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Doctor
+        fields = [
+            'id',
+            'name',
+            'department',
+            'specialization',
+            'visiting_fee',
+        ]
