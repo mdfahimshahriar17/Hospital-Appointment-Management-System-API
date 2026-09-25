53d403ed20645dcb78ed7e2d7a87ae72a52a5dda
fshahriar298 <mdfahimshahriar17@gmail.com>
2026-09-25 22:44:28 +0600

    Created view for doctor list

--- doctors/views.py
+++ doctors/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+from .models import Doctor
+from .serializers import DoctorSerializer
+
+
+class DoctorListCreateView(generics.ListCreateAPIView):
+    queryset = Doctor.objects.all()
+    serializer_class = DoctorSerializer
